31452c82746b4440967e7b297281aa5cde4c1a54
Alexander Martin <Alexander@theAlexanderMartin.com>
2016-08-05 14:32:16 -0400

    Initial commit

---
+++ src/org/usfirst/frc/team3641/robot/Robot.java
@@ -0,0 +1,33 @@
+
+package org.usfirst.frc.team3641.robot;
+
+import edu.wpi.first.wpilibj.IterativeRobot;
+
+public class Robot extends IterativeRobot {
+
+	public void robotInit()
+	{
+		
+	}
+	
+	public void autonomousInit()
+	{
+		
+	}
+
+	public void autonomousPeriodic()
+	{
+		
+	}
+
+	public void teleopPeriodic()
+	{
+		
+	}
+	
+	public void testPeriodic()
+	{
+		
+	}
+	
+}
